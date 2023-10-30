e85b3b5378c38fc6d8764b7c176b5191360ec3d6
EC2 Default User <ec2-user@ip-172-31-34-105.ap-northeast-1.compute.internal>
2023-10-30 08:52:45 +0000

    [corrected]function_of_follow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,21 +1,26 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
   
   def create
-    current_user.follow(params[:user_id])
+    user = User.find(params[:user_id])
+    current_user.follow(user)
+    redirect_to request.referer
   end
   
   def destroy
-    current_user.unfollow(params[:user_id])
+    user = User.find(params[:user_id])
+    current_user.unfollow(user)
+    redirect_to request.referer
   end
   
   def followings
     user = User.find(params[:user_id])
-    @users = user.following_users #Userモデルとのリレーションhas_manyで定義した名前？
+    @users = user.followings #Userモデルとのリレーションhas_manyで定義した名前？
   end
   
   def followers
     user＝User.find(params[:user_id])
-    @users = user.followed_users
+    @users = user.followers
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,16 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :following_users, through: :followers, source: :followed
-    #followersは架空のテーブル名
-  has_many :followeds, class_name: "Relationship", foregin_key: "followed_id", dependent: :destroy
-  has_many :followed_users, through: :followeds, source: :follower
+
+   # 自分がフォローされる（被フォロー）側の関係性
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 被フォロー関係を通じて参照→自分をフォローしている人
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+  # 自分がフォローする（与フォロー）側の関係性
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 与フォロー関係を通じて参照→自分がフォローしている人
+  has_many :followings, through: :relationships, source: :followed
 
   has_one_attached :profile_image
 
@@ -23,16 +28,16 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
-  
-  def follow(user_id)#フォローした時の処理
-    relationships.create(followed_id: user_id)
+
+  def follow(user)#フォローした時の処理
+    relationships.create(followed_id: user.id)
   end
-  
-  def unfollow(user_id)
-    relationships.find_by(followed_id: user_id).destroy
+
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
   end
-  
+
   def following?(user)
-    following_users.include?(user)
+    followings.include?(user)
   end
 end
